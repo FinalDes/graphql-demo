b34d8da867f633136ff917d3087910838a16cb8c
Alex Alonso <alex4551@gmail.com>
2017-11-08 22:00:50 +0800

    add eslint --fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import graphqlHTTP from 'express-graphql';
+import express from "express";
+import graphqlHTTP from "express-graphql";
 import {schema} from "./schema";
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000,function(){
-    console.log("Listening port 4000....")
+    // eslint-disable-next-line no-console
+    console.log("Listening port 4000....");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require('path');
+const path = require("path");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const fs = require("fs");
 
 const nodeModules = {};
 fs.readdirSync("node_modules")
-  .filter(x => [".bin"].indexOf(x) === -1)
-  .forEach((mod) => {
-    nodeModules[mod] = `commonjs ${mod}`;
-  });
+    .filter(x => [".bin"].indexOf(x) === -1)
+    .forEach((mod) => {
+        nodeModules[mod] = `commonjs ${mod}`;
+    });
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
-  },
-  externals: nodeModules,
-  module: {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    externals: nodeModules,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
-        }
-    ]
-  },
-  plugins: [
-    new MinifyPlugin(),
-  ],
+        ]
+    },
+    plugins: [
+        new MinifyPlugin(),
+    ],
 };
