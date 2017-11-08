db09003dceeb940237e7ec5f4077a719539bf787
Alex Alonso <alex4551@gmail.com>
2017-11-08 22:36:30 +0800

    add eslint and fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000,function(){
-  console.log("GraphQL Server is now running on http://localhost:4000/graphql");
+    // eslint-disable-next-line no-console
+    console.log("GraphQL Server is now running on http://localhost:4000/graphql");
 });
